ce0278802b9b914c9bf395207dba632ff6d7005f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-23 18:16:14 -0700

    feat(index): wrap index.ts in main function

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 import { Application, Graphics } from 'pixi.js'
 
-const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
-});
+	function main() {
+		const app = new Application({
+			view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+			resolution: window.devicePixelRatio || 1,
+			autoDensity: true,
+			backgroundColor: 0x6495ed,
+			width: 640,
+			height: 480
+		});
 
-const origin = {
-	x: app.screen.width / 2,
-	y: app.screen.height / 2
-};
+		const origin = {
+			x: app.screen.width / 2,
+			y: app.screen.height / 2
+		};
 
-const graphics: Graphics = new Graphics();
-graphics.beginFill(0xff0000);
-graphics.drawCircle(origin.x, origin.y, 50);
+		const graphics: Graphics = new Graphics();
+		graphics.beginFill(0xff0000);
+		graphics.drawCircle(origin.x, origin.y, 50);
 
 
-app.stage.addChild(graphics);
+		app.stage.addChild(graphics);
+	}
+
+window.addEventListener('load', main);
