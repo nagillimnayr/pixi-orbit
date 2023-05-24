49b3b719286a0ba5081d17f08820481eeab18345
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-23 18:12:05 -0700

    feat(Body): add 'color' member to Body class

--- src/modules/physics/Body.ts
+++ src/modules/physics/Body.ts
@@ -6,21 +6,26 @@ import { Graphics, IPoint, Point } from "pixi.js";
 class Body extends Graphics {
     mass: number;
     radius: number;
+    color: number;
 
     velocity: Point;
     acceleration: Point;
 
-    constructor({ x = 0, y = 0 , mass = 0, radius = 10, }) {
+    constructor({ x = 0, y = 0 , mass = 0, radius = 10, color = 0xffffff}) {
         super();
         this.mass = mass;
         this.radius = radius;
         this.x = x;
         this.y = y;
+        this.color = color;
+        this.beginFill(color);
         this.drawCircle(x, y, radius);
 
         this.velocity = new Point(0, 0);
         this.acceleration = new Point(0, 0);
     }
+
+
 
 
 }
